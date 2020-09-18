aa3a2b359d8ad2c4bb281703b64bf0f5be01bf06
ashok-sahu <ashoksahu1105@gmail.com>
2020-09-19 02:35:18 +0530

    changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [state,setState]=useState([])
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const result = await (await axios.get('/api/userlist')).data
+      console.log(result,'result')
+      setState(result.data)
+    }
+    fetchData()
+  },[])
   return (
     <div>
-      hello
+      {
+        state.map((elm,key)=>(
+          <div key={key}>
+            <h5>{elm.contactNum} - {elm.email}</h5>
+          </div>
+        ))
+      }
     </div>
   )
 }
